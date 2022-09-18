6e60e428a4c6d39640088e4d7822fdeae43b5e62
melnichukbohdan <melnichukbogdan1@gmail.com>
2022-09-18 13:04:56 +0300

    playlist https://www.youtube.com/playlist?list=PLu13hPnba_XCuinhx0D3H_O6Z6yHhOtp-
    video 7
    
    issues
    
    Added "display_settings"in Gallery Behavior  if formatter "photoswipe_field_formatter"

--- modules/custom/dlog_paragraphs/src/Plugin/paragraphs/Behavior/GalleryBehavior.php
+++ modules/custom/dlog_paragraphs/src/Plugin/paragraphs/Behavior/GalleryBehavior.php
@@ -38,15 +38,34 @@ class GalleryBehavior extends ParagraphsBehaviorBase {
                        Paragraph $paragraph,
                        EntityViewDisplayInterface $display,
                        $view_mode)  {
-    $images_per_row = $paragraph->getBehaviorSetting($this->getPluginId(),
-      'items_per_row' ,3);
+    $images_per_row = $paragraph->getBehaviorSetting($this->getPluginId(), 'items_per_row' ,4);
 
     //generating class name for bundle 'gallery'
-    $bem_block = 'paragraph-' . $paragraph->bundle() .
-      ($view_mode == 'default' ? '' : '-' . $view_mode);
-    $build['#attributes']['class'][] =
-      Html::getClass($bem_block  . '--images-per-row-' . $images_per_row);
+    $bem_block = 'paragraph-' . $paragraph->bundle() . ($view_mode == 'default' ? '' : '-' . $view_mode);
+    $build['#attributes']['class'][] = Html::getClass($bem_block  . '--images-per-row-' . $images_per_row);
+
+    if (isset($build['field_gallery']) && $build['field_gallery']['#formatter'] == 'photoswipe_field_formatter') {
+      switch ($images_per_row) {
+        case '4' :
+        default:
+          $image_style = 'paragraph_gallery_image_3_of_12';
+          break;
+
+        case '3' :
+          $image_style = 'paragraph_gallery_image_4_of_12';
+          break;
+
+        case '2' :
+          $image_style = 'paragraph_gallery_image_6_of_12';
+          break;
+      }
+
+      for ($i = 0; $i <= count($build['field_gallery']['#items']); $i++) {
+        $build['field_gallery'][$i]['#display_settings']['photoswipe_node_style'] = $image_style;
+      }
+    }
   }
+
 
   /**
    * {@inheritDoc}
@@ -58,15 +77,11 @@ class GalleryBehavior extends ParagraphsBehaviorBase {
       '#type' => 'select',
       '#title' => $this->t('Number of images per row'),
       '#options' => [
-        '2' => $this->formatPlural('2', '1 photo per row',
-          '@count photos per row'),
-        '3' => $this->formatPlural('3', '1 photo per row',
-          '@count photos per row'),
-        '4' => $this->formatPlural('4', '1 photo per row',
-          '@count photos per row'),
+        '2' => $this->formatPlural('2', '1 photo per row', '@count photos per row'),
+        '3' => $this->formatPlural('3', '1 photo per row', '@count photos per row'),
+        '4' => $this->formatPlural('4', '1 photo per row', '@count photos per row'),
       ],
-      '#default_value' => $paragraph->getBehaviorSetting($this->getPluginId(),
-        'items_per_row' ,3),
+      '#default_value' => $paragraph->getBehaviorSetting($this->getPluginId(), 'items_per_row' ,4),
     ];
 
     return $form;
